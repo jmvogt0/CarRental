c7e1b7df4af689a38221f01034c8e88859f8c18a
Jan Vogt <j.vogt@marks.local>
2023-06-01 16:57:48 +0200

    deactivated serializableCheck to prevent errors

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistedReducer, persistStore, serializableMiddleware } from '../store/persistConfig';
 
+const customizedMiddleware = [];
+
 const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(serializableMiddleware),
+    getDefaultMiddleware({
+      serializableCheck: false, // Deaktiviere die Serialisierungsüberprüfung
+    }).concat(customizedMiddleware),
 });
 
 const persistor = persistStore(store);
